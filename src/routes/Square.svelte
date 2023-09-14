f5c21e08fa44f7605a5f01625ff63cab29f1f1de
Tejus Parikh <tejus@vijedi.net>
2023-09-14 19:44:27 +0000

    Adding support and styling for selection

--- src/routes/Square.svelte
+++ src/routes/Square.svelte
@@ -1,8 +1,13 @@
-<script>
-	export let content;
+<script lang="ts">
+	export let content: Square;
+
+	let selected: boolean = false;
+	function toggle() {
+		selected = !selected;
+	}
 </script>
 
-<button type="button">
+<button type="button" on:click={toggle} class:selected>
 	<div class="button-content">
 		{content.text}
 	</div>
@@ -13,19 +18,31 @@
 		background: transparent;
 		border: 0;
 		margin: 0;
-		padding: 0;
-		align-items: center;
+		padding: 4px;
 		height: 100%;
 		width: 100%;
-		display: block;
-		overflow-wrap: break-word;
+		cursor: pointer;
+	}
+
+	button.selected .button-content {
+		background: #ee8c1188;
+		border-color: #ee8c11;
 	}
 
 	.button-content {
 		text-align: center;
 		flex-shrink: 0;
 		padding: 0.1em;
-		font-size: 1.4em;
+		font-size: 1.2em;
 		overflow-wrap: break-word;
+		max-height: calc(100vmin / 5);
+		height: calc(100% - 9px);
+		width: calc(100% - 9px);
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		border: 3px solid transparent;
+		border-radius: 50%;
+		overflow: hidden;
 	}
 </style>
